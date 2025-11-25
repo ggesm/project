53a0c27eba305d3f87f6834cf06155be41d943d8
ggesm <giana.gesmundo@gmail.com>
2025-11-24 21:58:06 -0800

    layout 1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Wait until the page fully loads
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("clickMeBtn");
+  
+    button.addEventListener("click", () => {
+      alert("Button clicked!");
+    });
+  });
+  
